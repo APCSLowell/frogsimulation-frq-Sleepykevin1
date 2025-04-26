9711df2c69311ca1897264bf58c2ac4669678de3
Sleepykevin1 <Hunterkevin12398@gmail.com>
2025-04-25 22:54:13 -0700

    Update FrogSimulation.java

--- src/main/java/FrogSimulation.java
+++ src/main/java/FrogSimulation.java
@@ -2,40 +2,39 @@ public class FrogSimulation
 {
     private int goalDistance;
     private int maxHops;
-
+    
     public FrogSimulation(int dist, int numHops)
     {
         goalDistance = dist;
         maxHops = numHops;
+        maxHopsHolder = numHops;  //ignore this, it's used for testing
     }
-
+    
+    //private int hopDistance() 
+    //implementation is below, but not important
+    
     public boolean simulate()
     { 
-        int position = 0;
-
-        for (int hop = 0; hop < maxHops; hop++) {
-            position += hopDistance();
-
-            if (position >= goalDistance) {
-                return true;
-            } else if (position < 0) {
+        int total = 0;
+        int hops = 0;
+        while(hops<maxHops){
+            total+=hopDistance();
+            hops++;
+            if(total<0)
                 return false;
-            }
+            else if (total>=goalDistance)
+                return true;
         }
-
         return false;
     }
-
+    
     public double runSimulations(int num)
     { 
-        int successes = 0;
-
-        for (int i = 0; i < num; i++) {
-            if (simulate()) {
-                successes++;
-            }
-        }
-
-        return (double) successes / num;
+        double ratio = 0;
+        for(int i = 0;i<num;i++)
+            if(simulate()==true)
+                ratio++;
+        return ratio/num;
+        
     }
-}
+    }
